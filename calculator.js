e172915ca056e9b5cc6892d09e200eb941bfa4c8
M00setheM1ghty <129610686+M00setheM1ghty@users.noreply.github.com>
2023-05-09 11:19:07 +0200

    add allclear and delete functions and link them

--- calculator.js
+++ calculator.js
@@ -4,26 +4,38 @@
 let currentNums = document.querySelector('[data-current-nums]');
 let prevNums = document.querySelector('[data-prev-nums]');
 /* operands and number buttons*/
-let dataOperands = document.querySelectorAll('[data-operand]');
-let numberButtons = document.querySelectorAll('[data-number]');
+let dataOperand = document.querySelectorAll('[data-operand]');
+let numberButton = document.querySelectorAll('[data-number]');
 
 /*del and AC buttons*/
 let delBtn = document.querySelector('[data-delete]');
 let allClearBtn = document.querySelector('[data-clearall]');
 
-/* required functions */
-/*1. clear display: */
-function clearDisplay(){
-    currentNums = '';
-    prevNums = '';
-}
-
-/*. add numbers to display */
-function addNums(){
-    numberButtons.forEach.addEventlistener('click', ()=>{
-        currentNums.append(numberButtons.innerText);
+/* eventListeners + functionality */
+/*add event listener to all number buttons and append number to display */
+numberButton.forEach(button => {
+    button.addEventListener('click', () => {
+        currentNums.innerHTML += button.innerText
     })
+})
+
+/* link clearDisplay() to AC button */
+allClearBtn.addEventListener('click', clearDisplay);
+/*link deleteLast() to del button */
+delBtn.addEventListener('click', deleteLast);
+
+/* required functions */
+/*clear display: */
+function clearDisplay() {
+    currentNums.innerHTML = '';
+    prevNums.innerHTML = '';
 }
+/* delete last entered num (DEL button function) */
+function deleteLast() {
+    currentNums.innerHTML = currentNums.innerHTML.slice(1, -1);
+}
+/*. add numbers to display */
+
 
 /* add operand to display */
 
@@ -31,6 +43,4 @@ function addNums(){
 
 /* solve the given equation */
 
-/* delete last entered num (DEL button function) */
 
-/* link clear display to AC button */
