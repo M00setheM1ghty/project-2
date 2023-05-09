618edeae6f58a5957a771c1f2cfc6635e39ca084
M00setheM1ghty <129610686+M00setheM1ghty@users.noreply.github.com>
2023-05-09 09:48:46 +0200

    defined variables for calculator elements

---
+++ calculator.js
@@ -0,0 +1,13 @@
+/* define elements */
+
+/* the numbers that will be shown in the output div*/
+let currentNums = document.querySelector('[data-current-nums]');
+let prevNums = document.querySelector('[data-prev-nums]');
+/* operands and number buttons*/
+let dataOperands = document.querySelectorAll('[data-operand]');
+let numberButtons = document.querySelectorAll('[data-number]');
+
+/*del and AC buttons*/
+let delBtn = document.querySelector('[data-delete]');
+let allClearBtn = document.querySelector('[data-clearall]');
+
